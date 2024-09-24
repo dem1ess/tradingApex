aaa6e8d7382b7560464dd81972b225a7d09085b6
Kharchenko Stanislav <stanislav_kharchenko@outlook.com>
2024-09-24 11:43:03 +0300

    Update api.js

--- clientApex/src/services/api.js
+++ clientApex/src/services/api.js
@@ -1,9 +1,6 @@
 import axios from 'axios'
 import { getTokenFromLocalStorage } from '../helpers/localstorage.helper'
 
-// export const instance = axios.create({
-//   baseURL: 'https://api.apexfinance.io/api',
-// });
 
 export const instance = axios.create({
 	baseURL: 'http://localhost:3001/api',
